d7f666e78f815fb2f5a5b80ce33b1a87facd417c
Bruno Muciño <mucinoab@gmail.com>
2019-10-14 10:56:50 -0500

    Se mejora bisección y se agrega libreria para manejar expersiones.

--- Biseccion.cpp
+++ Biseccion.cpp
@@ -1,29 +1,56 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include "exprtk.hpp"
 
 using namespace std;
+
+
+void intervalos(double *a, double *b){
+  cout << "Introduzca primer intervalo:" << '\n';
+  cin >> *a;
+  cout << "Introduzca segundo intervalo:" << '\n';
+  cin >> *b;
+
+}
+
+double func(string expression_str, double x){
+
+  typedef exprtk::symbol_table<double> symbol_table_t;
+  typedef exprtk::expression<double>     expression_t;
+  typedef exprtk::parser<double>             parser_t;
+
+  // Register x with the symbol_table
+  symbol_table_t symbol_table;
+  symbol_table.add_variable("x",x);
+
+  // Instantiate expression and register symbol_table
+  expression_t expression;
+  expression.register_symbol_table(symbol_table);
+
+  // Instantiate parser and compile the expression
+  parser_t parser;
+  parser.compile(expression_str,expression);
+
+  return expression.value();
+}
+
 
 double error = 0.0000001;
 double c;
 
-double func(double x){
-  // aqui algo para poder convertit strings a funciones calculabes
-  return exp(-x)-x;
-}
-
-void biseccion(double a, double b){
-  if((func(a) * func(b)) >= 0){
+void biseccion(double a, double b, string s){
+  if((func(s, a) * func(s, b)) >= 0){
       cout<<"Dentro de ese intervalo no existe raíz real.";
       return;
   }
   c = a;
   while ((b-a) >= error){
       c = (a+b)/2;
-      if (func(c) == 0.0)
+      if (func(s, c) == 0.0)
           break;
       else
-        if ((func(c)*func(a)) < 0)
+        if ((func(s, c)*func(s, a)) < 0)
           b = c;
         else
           a = c;
@@ -32,9 +59,16 @@ void biseccion(double a, double b){
 
 
 int main (){
-  double a = 0, b = 2; //intervalos
+  double a, b; //intervalos
 
-  biseccion(a, b);
+
+  string expression_str;
+  cout << "Ingrese la expression algebraica a evaular:" << '\n';
+  cin >> expression_str;
+
+  intervalos(&a, &b);
+
+  biseccion(a, b, expression_str);
   cout << c << '\n';
 
   return 0;
